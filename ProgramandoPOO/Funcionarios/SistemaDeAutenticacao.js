51bde232758df5c30aea85f9e808f4474f10bea3
Marcelo Tadini <tadini.marcelo53@gmail.com>
2021-03-13 11:07:54 -0300

    Verificando tipos e propriedades no sistema de autenticacao

--- ProgramandoPOO/Funcionarios/SistemaDeAutenticacao.js
+++ ProgramandoPOO/Funcionarios/SistemaDeAutenticacao.js
@@ -1,5 +1,16 @@
+/*
+ Ser autenticavel significa ter a propriedade "senha"
+*/
+
 export class SistemaDeAutenticacao{
-    static login(funcionario, senha){
-        return funcionario.senha == senha;
+    static login(autenticavel, senha){
+        if(SistemaDeAutenticacao.ehAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static ehAutenticavel(autenticavel){
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
     }
 }
--- ProgramandoPOO/index.js
+++ ProgramandoPOO/index.js
@@ -9,7 +9,12 @@ diretor.cadastrarSenha("123456");
 const gerente = new Gerente("Ana", 20000, 43234265566);
 gerente.cadastrarSenha("456789");
 
-const estaLogado =  SistemaDeAutenticacao.login(gerente, "456789");
+const cliente = new Cliente("Joao", 5543534533, "098");
 
-console.log(estaLogado);
+const gerenteEstaLogado =  SistemaDeAutenticacao.login(gerente, "456789");
+const diretorEstaLogado =  SistemaDeAutenticacao.login(diretor, "123456");
+const clienteEstaLogado = SistemaDeAutenticacao.login(cliente, "098");
+
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
 
