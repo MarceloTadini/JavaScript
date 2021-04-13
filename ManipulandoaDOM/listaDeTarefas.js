10b50a095877f8da2317192ae74bc40c7d0ef260
Marcelo Tadini <tadini.marcelo53@gmail.com>
2021-04-13 16:00:11 -0300

    Adicionando as tarefas um embaixo do outro

--- ManipulandoaDOM/listaDeTarefas.js
+++ ManipulandoaDOM/listaDeTarefas.js
@@ -2,12 +2,16 @@ const criarTarefa = (evento)=> {
     evento.preventDefault(); //evita o recarregamento da página
     
     const inputTarefa = document.querySelector('[data-form-input]');
+    const lista = document.querySelector('[data-list]');
     const valor = inputTarefa.value; //pega o valor do inputTarefa
 
-    const tarefa = document.querySelector('[data-tesk]');
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task'); //Adicionando classe a lista
     const conteudo = `<p class="content">${valor}</p>`; 
 
-    tarefa.innerHTML = conteudo; //colocando o conteudo na tarefa
+    tarefa.innerHTML = conteudo; //Atribuindo conteúdo
+    lista.appendChild(tarefa); //Declarando a lista como pai de p
+
 
     inputTarefa.value = ""; //Limpando o input
 } 
