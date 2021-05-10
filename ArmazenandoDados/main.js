ae535d065b7a57468a76eee896dd32c2b0d636f6
Marcelo Tadini <tadini.marcelo53@gmail.com>
2021-05-10 18:22:35 -0300

    Refatoração ad função main.js e uso de LocalStorage

--- ArmazenandoDados/main.js
+++ ArmazenandoDados/main.js
@@ -1,41 +1,4 @@
-import BotaoConclui from './componentes/concluiTarefa.js';
-import BotaoDeleta from './componentes/deletaTarefa.js';
- 
-const handleNewItem = (evento) => {
-    evento.preventDefault();
-
-    const lista = document.querySelector('[data-list]');
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
-
-    const calendar = document.querySelector('[data-form-date]');
-    const date = moment(calendar.value); //Recebe o valor do input da data a lib moment
-    const dateFormat = date.format('DD/MM/YYYY');
-
-    const datas = {
-        valor, 
-        dateFormat
-    }
-
-    const criaTarefa = criarTarefa(datas);
-
-    lista.appendChild(criaTarefa);
-    input.value = " ";
-}
-
-const criarTarefa = ({valor, dateFormat}) => {
-
-    const tarefa = document.createElement('li');
-    tarefa.classList.add('task');
-    const conteudo = `<p class="content">${dateFormat} * ${valor}</p>`;
-
-    tarefa.innerHTML = conteudo;
-
-    tarefa.appendChild(BotaoConclui());
-    tarefa.appendChild(BotaoDeleta());
-    
-    return tarefa;
-}
+import {handleNewItem} from './componentes/criaTarefa.js';
 
 const novaTarefa = document.querySelector('[data-form-button]');
 
